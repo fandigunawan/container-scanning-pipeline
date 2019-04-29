12994e03545fc0f11bbe1782005411b1b9ff0260
Administrator <admin@example.com>
2019-04-29 21:25:16 +0000

    Deleted gitlab/README.md, gitlab/freeipa_settings.yml, gitlab/gitlab_install.sh, opensource/prometheus/README.md, opensource/prometheus/example_prometheus_configuration.yml files

--- gitlab/gitlab_install.sh
+++
@@ -1,5 +0,0 @@
-sudo yum install -y curl policycoreutils-python openssh-server
-sudo systemctl enable sshd
-sudo systemctl start sshd
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
-sudo EXTERNAL_URL="https://<URL_HERE>" yum install -y gitlab-ce
