48fb434949328d2b5cc3e49a94d2707a6225c018
kyle <krafaels@redhat.com>
2019-09-20 14:34:49 -0400

    appends log file in scanning repo, need to append to README in container repo

--- status-update.py
+++ status-update.py
@@ -1,31 +1,35 @@
 import gitlab
 import sys
-# import importlib
-# importlib.import_module('gitlab-class.py')
+import datetime
+
+# USAGE: python status-update.py sys.argv[0]
 
 dccscr = 'https://dccscr.dsop.io/'
-# usage: python status-update.py sys.argv[0]
+state = sys.argv[1]
+build_id = sys.argv[2]
 
 #get image name from jenkins - will need this to determine which project the status report will be sent in gitlab
 imageName = 'from jenkins pipeline'
 
-if sys.argv[0] == 'FAILED':
-    print('FAILED')
-if sys.argv[0] == 'SUCCESS':
-    print('SUCCESS')
+if state == 'FAILED':
+    file = open("build-status.log", 'a')
+    print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Status Update:', str(state), str(build_id), file=file)
 
+if state == 'SUCCESS':
+    file = open("build-status.log", 'a')
+    print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Status Update:', str(state), str(build_id), file=file)
 
-def return_auth():
-    g1 = gitlab.Gitlab(dccscr, private_token='XQ3yQ7RP9LXSLEAAA9jK')
-    return g1
-
-
-def get_projects():
-    g1 = return_auth()
-    g1.auth()
-    projects = g1.projects.list()
-    for project in projects:
-        print(project)
+# def return_auth():
+#     g1 = gitlab.Gitlab(dccscr, private_token='XQ3yQ7RP9LXSLEAAA9jK')
+#     return g1
+#
+#
+# def get_projects():
+#     g1 = return_auth()
+#     g1.auth()
+#     projects = g1.projects.list()
+#     for project in projects:
+#         print(project)
 
 
 # def get_current_proj(imageName):
